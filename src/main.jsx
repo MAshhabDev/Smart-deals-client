e06e1870256163c4898255afeb125b30097ed38d
Mahir4098 <mahirjr98@gmail.com>
2026-04-14 19:19:19 +0600

    Implement Navbar and Home Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
+import { Children, Component, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import AuthProvider from './Context/AuthProvider.jsx';
+import MainLayout from './Layouts/MainLayout.jsx';
+import Home from './Components/Home/Home.jsx';
+import AllProducts from './Components/AllProducts/AllProducts.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      },
+      {
+        path: "allProducts",
+        Component: AllProducts
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
 )
